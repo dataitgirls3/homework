3066320562083bb6d5df4743402a644be3ac0b85
전재민 <3441jj@gmail.com>
2019-09-28 20:11:01 +0900

    homework01_jaem1n

---
+++ homework01/solutions/homework01_jaem1n.py
@@ -0,0 +1,12 @@
+def add_1(x):
+    return [i+1 for i in x]
+
+def square(x):
+    return [i**2 for i in x]
+
+def filter_by_length(x, n):
+    return [i for i in x if len(i) == n]
+
+def without_vowels(x):
+    vowels = 'aeiou'
+    return ''.join([i for i in x if i.lower() not in vowels])
