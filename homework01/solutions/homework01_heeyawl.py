3ea903e204cdedfde2a8891d614428902e8dcf89
Heeyawl Yu <heeyawl@gmail.com>
2019-10-13 17:28:50 +0900

    I don't see why not

--- homework01/solutions/homework01_heeyawl.py
+++ homework01/solutions/homework01_heeyawl.py
@@ -1,18 +1,20 @@
-
-#homework01_heeyawl
-
 def add_1(x):
     return [i+1 for i in x]
 
+
 assert add_1([1, 2, 3]) == [2, 3, 4]
+
 
 def square(x):
     return [i ** 2 for i in x]
 
+
 assert square([1, 2, 3]) == [1, 4, 9]
+
 
 def filter_by_length(x,n):
     return [i for i in x if len(i) == n]
+
 
 assert filter_by_length(['hi', 'mate'], 4) == ['mate']
 assert filter_by_length(['hi', 'mate'], 1) == []
