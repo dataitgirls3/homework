34adf6479869d267b8f784c6ae02cc202f396464
leebomin <codavincee@gmail.com>
2019-10-17 00:08:27 +0900

    add filter_by_length() function

--- homework01/solutions/homework01_springkind.py
+++ homework01/solutions/homework01_springkind.py
@@ -4,8 +4,12 @@ def add_1(l):
 def square(l):
     return [x ** 2 for x in l]
 
-def filter_by_length(x):
-    pass
+def filter_by_length(l, n):
+    filtered = []
+    for x in l:
+        if len(x) == n:
+            filtered.append(x)
+    return filtered
 
 def without_vowels(x):
     pass
