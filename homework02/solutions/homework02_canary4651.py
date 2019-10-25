9a3194d511d96f457cb382c3ceda8eb377486c11
YE H <canary4651@gmail.com>
2019-10-25 23:01:58 +0900

    homework02_canary4651

--- homework02/solutions/homework02_canary4651.py
+++ homework02/solutions/homework02_canary4651.py
@@ -3,18 +3,18 @@ def variance(x):
     mean = sum(x)/len(x)
     return sum((i - mean)**2 for i in x)/len(x)
 
-#6
+# 6
 import math
 
 def stddev(x):
     return math.sqrt(variance(x))
 
-#7  딕셔너리의 키와 값을 바꾸는 함수
+# 7  딕셔너리의 키와 값을 바꾸는 함수
 def invert(x):
     return {v:k for k,v in x.items()}
 
 
-#8 : 두 리스트를 하나의 딕셔너리로 합쳐주는 함수
+# 8 : 두 리스트를 하나의 딕셔너리로 합쳐주는 함수
 def to_dict(x, y):
     return {k:v for k,v in zip(x,y)}
 
