8f0a153bb03da5221edefb88c745be4fdaf8ae9d
YE H <canary4651@gmail.com>
2019-10-25 13:17:31 +0900

    homewokr01_canary4651

--- homework01/solutions/homework01_canary4651.py
+++ homework01/solutions/homework01_canary4651.py
@@ -2,23 +2,18 @@
 def add_1(x):
     return [i+1 for i in x]
 
-print(add_1([1, 2, 3]))
 
 #2
 def square(x):
     return [i**2 for i in x]
 
-print(square([1, 2, 3]))
 
 #3
 def filter_by_length(x, n):
     return [i for i in x if len(i) == n]
 
-print(filter_by_length(['hi', 'mate'], 4))
 
 #4
 def without_vowels(x):
-    vowels = ['a','i','u','e','o']
-    return ''.join([x for x in x if x not in vowels])
-
-print(without_vowels('dataitgirls3'))
+    vowels = 'aeiou'
+    return ''.join([i for i in x if i.lower() not in vowels])
